1ab0aa26d64356e07c00d868c33d8124c3c44190
Andrew Jarombek <ajarombek95@gmail.com>
2016-11-18 16:43:17 -0500

    Welsh Powell Algorithm Done! (1/4)

--- GraphTest.py
+++ GraphTest.py
@@ -3,17 +3,25 @@
 # A test suite for the Graph class
 
 from Graph import Graph
+from GraphColorings import GraphColorings
 
 def test():
 
     # Create a Graph
     vertexSet = {1,2,3}
     edgeSet = {(1,2), (2,3)}
-    graph = Graph(vertexSet, edgeSet)
+    graphcoloring = GraphColorings(vertexSet, edgeSet)
 
     # Inspect the Graph
-    for v in graph.vertices():
-        print(v, "->", graph.neighborsOf(v))
+    for v in graphcoloring.graph.vertices():
+        print(v, "->", graphcoloring.graph.neighborsOf(v))
+
+    # Test the Welsh-Powell Graph Coloring Algorithm
+    coloring = graphcoloring.welshPowell()
+    print("Welsh-Powell Graph Coloring:")
+    for v in coloring:
+        print(v[0], "-->", v[1])
+
 
 if __name__ == '__main__':
     test()
